566353733418cfd218ad57b87b8b469c4abca779
Ayodeji Carew <77218244+carewayo@users.noreply.github.com>
2022-03-12 15:35:13 -0800

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Stop all servers and start the server as a daemon
-sudo npm install -g pm2
+sudo npm i -g pm2
+#sudo npm install -g pm2
 pm2 stopall
 pm2 start npm --start
 #forever start --minUptime 10000 --spinSleepTime 1000 /home/ec2-user/nodejs/app.js
