f633e094322044f4daa9bd0fbe181ea17802e339
Ayodeji Carew <77218244+carewayo@users.noreply.github.com>
2022-03-12 17:13:41 -0800

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #delete existing nodejs file if exist
-sudo rm -rf /home/ubuntu/nodejs
-sudo mkdir /home/ubuntu/nodejs
-cd /home/ubuntu/nodejs
-sudo apt update
-sudo apt install nodejs
+#sudo rm -rf /home/ubuntu/nodejs
+#sudo mkdir /home/ubuntu/nodejs
+cd /home/ubuntu/portfolio-app
+#sudo apt update
+#sudo apt install nodejs
 sudo npm install
-npm install
+#npm install
 
 npm start
