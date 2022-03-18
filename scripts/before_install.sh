9999c92a5f76ea11ae062c37a19b201794c8ca9c
Ayodeji Carew <77218244+carewayo@users.noreply.github.com>
2022-03-17 19:42:57 -0700

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # Install node.js  
-sudo apt-get update
-sudo apt-get install gcc g++ make
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install nodejs -y
-sudo npm install
-sudo npm install -g pm2
+sudo yum install -y gcc-c++ make 
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+sudo yum update
+sudo yum install -y nodejs
+
+# Install nodemon
+# sudo npm install nodemon -g
+npm install
 # Install forever module 
 # https://www.npmjs.com/package/forever
-#sudo npm install pm2 -g
+sudo npm install forever -g
